f98d0c4d15961fb10c1c79b02ce9b229b263385e
VV <visakhvinod2004@gmail.com>
2026-06-07 17:56:52 +0530

    Deleted a file

--- test_internals.sql
+++
@@ -1,6 +0,0 @@
-SELECT im.course_assignment_id, c.code, dc.period_number
-FROM internal_marks im
-JOIN course_assignments ca ON im.course_assignment_id = ca.id
-JOIN courses c ON ca.course_id = c.id
-LEFT JOIN department_courses dc ON dc.course_code = c.code 
-LIMIT 5;
